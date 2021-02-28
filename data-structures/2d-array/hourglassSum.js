63f94c494f7f2322c8e41537ee13d8987b86bdae
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-02-28 13:28:06 -0300

    refactor: change logic

--- data-structures/2d-array/hourglassSum.js
+++ data-structures/2d-array/hourglassSum.js
@@ -2,7 +2,7 @@
 function hourglassSum(arr) {
   const sizeLines = (arr || []).length - 2
   const lineSize = arr[0].length -2
-  let highestSum = null;
+  let highestSum = -Infinity;
 
   for (let index = 0; index < sizeLines; index++) {
     for (let elemIndex = 0; elemIndex < lineSize; elemIndex++) {
@@ -11,13 +11,9 @@ function hourglassSum(arr) {
       const foot = arr[index + 2].slice(elemIndex, 3 + elemIndex)
       
       const sum = [...head, middle, ...foot].reduce((acc, item) => acc+ item, 0)
-      highestSum = highestSum !== null
-        ? (
-          sum > highestSum
-            ? sum
-            : highestSum
-          )
-        : sum
+      highestSum = sum > highestSum
+        ? sum
+        : highestSum 
     }
   }
   return highestSum
