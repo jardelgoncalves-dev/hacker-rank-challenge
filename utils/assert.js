31127c6c2f5376095f3df6a08d8ee9478c9c0356
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-02-28 15:32:17 -0300

    refactor: change to show message error

--- utils/assert.js
+++ utils/assert.js
@@ -11,6 +11,7 @@ exports.it = (message, fn) => {
     console.log(`\x1b[32m  ✓ ${message}\x1b[0m`)
   } catch(error) {
     console.log(`\x1b[31m  ✘ ${message}\x1b[0m`)
+    console.log(`\n${error.message}`)
     throw error
   }
 }
