ec6224c81c1f2c1f2fea0b2f87b99b9762e8d191
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-02-27 21:36:56 -0300

    feat: add assert to test

---
+++ utils/assert.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+
+exports.describe = (message, fn) => {
+  console.log(message)
+  fn()
+}
+
+exports.it = (message, fn) => {
+  try {
+    fn()
+    console.log(`\x1b[32m  ✓ ${message}\x1b[0m`)
+  } catch(error) {
+    console.log(`\x1b[31m  ✘ ${message}\x1b[0m`)
+    throw error
+  }
+}
+
+exports.expect = (expected) => {
+  return {
+    toEqual: (actual) => {
+      assert.deepStrictEqual(actual, expected)
+    }
+  }
+}
