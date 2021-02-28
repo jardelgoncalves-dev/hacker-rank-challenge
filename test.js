46230a7b268f3a1f224d4b5afd51386177a7f7df
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-02-28 13:21:01 -0300

    feat: add regex to filter file

--- test.js
+++ test.js
@@ -4,18 +4,21 @@ const childProccess = require('child_process');
 
 const isDirectory = dir => fs.statSync(dir).isDirectory();
 const getDirectories = dirs =>
-    fs.readdirSync(dirs).map(name => path.join(dirs, name)).filter(isDirectory);
+  fs.readdirSync(dirs).map(name => path.join(dirs, name)).filter(isDirectory);
 
 const isFile = file => fs.statSync(file).isFile();  
 const getFiles = files =>
-    fs.readdirSync(files).map(name => path.join(files, name)).filter(isFile);
+  fs.readdirSync(files).map(name => path.join(files, name)).filter(isFile);
 
 const getFilesRecursively = (paths) => {
-    let dirs = getDirectories(paths);
-    let files = dirs
-        .map(dir => getFilesRecursively(dir)) 
-        .reduce((a,b) => a.concat(b), []);
-    return files.concat(getFiles(paths)).filter(f => /test.js$/.test(f));
+  const [match] = process.argv.slice(2)
+  let dirs = getDirectories(paths);
+  let files = dirs
+      .map(dir => getFilesRecursively(dir)) 
+      .reduce((a,b) => a.concat(b), []);
+  return files.concat(getFiles(paths))
+    .filter(f => /test.js$/.test(f))
+    .filter(f => !match || new RegExp(match, 'i').test(f));
 };
 
 const directories = ['data-structures']
