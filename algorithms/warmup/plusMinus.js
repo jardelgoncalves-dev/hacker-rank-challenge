97fa5e41b9019277775fed0c0fded70fc01e2430
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-03-21 19:11:22 -0300

    feat: add solution to plusMinus

---
+++ algorithms/warmup/plusMinus.js
@@ -0,0 +1,28 @@
+// Complete the plusMinus function below.
+function plusMinus(arr) {
+    
+  const counters = arr.reduce((counter, number) => {
+     return {
+         negative: number < 0 ? counter.negative+1: counter.negative,
+         zero: number === 0 ? counter.zero+1 : counter.zero,
+         positive: number > 0 ? counter.positive+1 : counter.positive
+     } 
+  }, {negative: 0, zero: 0, positive: 0})
+  
+  const size = arr.length
+
+  const toFixed = (result) => (result).toFixed(6)
+  
+  // process.stdout.write(
+  //   toFixed(counters.positive/size) + '\n' +
+  //   toFixed(counters.negative/size) + '\n' +
+  //   toFixed(counters.zero/size)
+  // )
+  return [
+      toFixed(counters.positive/size),
+      toFixed(counters.negative/size),
+      toFixed(counters.zero/size)
+  ]
+}
+
+module.exports = plusMinus
